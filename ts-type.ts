f2cc5154d05751d6029f3c900d63eb90ef283950
김재윤 <jaeyun1723@naver.com>
2024-04-29 02:08:47 +0900

    lecture 3

---
+++ ts-type.ts
@@ -0,0 +1,18 @@
+let str: string = "hello";
+let num: number = 10;
+let arr: Array<number> = [1, 2, 3];
+let heros: Array<string> = ["Capt", "Thor", "Hulk"];
+let items: number[] = [1, 2, 3];
+
+// TS 튜플
+let address: [string, number] = ["ga", 100];
+
+// TS 객체
+let obj: object = {};
+
+let person: { name: string; age: number } = {
+  name: "thor",
+  age: 1000,
+};
+
+let show: boolean = true;
